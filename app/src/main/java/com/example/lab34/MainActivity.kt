a077b036fa6da08ff125628e61c4bcb0f3908036
TeacherLoc <zzminhrauzz@gmail.com>
2024-10-02 19:53:13 +0700

    first commit

---
+++ app/src/main/java/com/example/lab34/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.lab34
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Liên kết các thành phần giao diện
+        val textViewContainer: LinearLayout = findViewById(R.id.textViewContainer)
+        val addFromXmlButton: Button = findViewById(R.id.addFromXmlButton)
+        val clearButton: Button = findViewById(R.id.clearButton)
+
+        // LayoutInflater để nạp giao diện từ XML
+        val inflater = LayoutInflater.from(this)
+
+        // Xử lý sự kiện nhấn nút "Thêm thành phần"
+        addFromXmlButton.setOnClickListener {
+            val textView = inflater.inflate(R.layout.textview_layout, textViewContainer, false)
+            textViewContainer.addView(textView) // Thêm TextView từ XML vào
+
+        }
+// Xử lý sự kiện nhấn nút "Xóa tất cả"
+        clearButton.setOnClickListener {
+            textViewContainer.removeAllViews() // Xóa tất cả các TextView đã thêm
+        }
+    }
+}
